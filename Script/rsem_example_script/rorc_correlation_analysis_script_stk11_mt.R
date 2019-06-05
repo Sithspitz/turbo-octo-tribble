7490e3de7deed9be8303faa6ea48ed2b7ec9750e
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-06-05 11:18:26 +0100

    Upload example correlation scripts for RSEM and Zscore analysis.

---
+++ Script/rsem_example_script/rorc_correlation_analysis_script_stk11_mt.R
@@ -0,0 +1,60 @@
+##########################################################
+##--------------RORC Correlation Analysis---------------##
+##########################################################
+
+# Library
+library(ggpubr)
+library(dplyr)
+
+
+## Other Mutation group RORC correlations ##
+
+# Import
+setwd("~/DataShare/rorc_correlations/expression_data_by_mutation/stk11_mt_rsem/")
+rorc_stk11_mt <- read.csv("rorc_stk11_mt.csv", row.names = 1, header = F)
+stk11_mt <- read.csv("stk11_mt.csv", row.names = 1, header = F)
+
+# Remove rows with only 0 in them
+other_mt <- stk11_mt[apply(stk11_mt[,-1], 1, function(x) !all(x==0)),]
+
+# Numerisise RORC data and query data
+## Keep 'data_names' df for rownames
+rorc_other2 <- as.numeric(rorc_stk11_mt)
+other_mt2 <- as.matrix(other_mt)
+data_names <- other_mt
+
+# For Loop Correlation Analysis Pearson
+correlations <- vector()
+for (i in 1:nrow(other_mt2)) {
+  print(rownames(data_names[i, ]))
+  row <- other_mt2[i, ]
+  corr_test <- cor.test(rorc_other2, row, method = "pearson")
+  if(corr_test$p.value <=0.05) {
+    correlations[length(correlations)+1] <- rownames(data_names[i, ])
+    correlations[length(correlations)+1] <- corr_test$p.value
+    }
+}
+
+sig_corr <- as.data.frame(correlations)
+
+setwd("~/DataShare/rorc_correlations/Output/stk11_mt_rsem/")
+write.csv(sig_corr, "stk11_mt_sig_rorc_correlations_pearson.csv", row.names = F)
+
+# For Loop Correlation Analysis Spearman
+rm(correlations, sig_corr)
+
+correlations <- vector()
+for (i in 1:nrow(other_mt2)) {
+  print(rownames(data_names[i, ]))
+  row <- other_mt2[i, ]
+  corr_test <- cor.test(rorc_other2, row, method = "spearman")
+  if(corr_test$p.value <=0.05) {
+    correlations[length(correlations)+1] <- rownames(data_names[i, ])
+    correlations[length(correlations)+1] <- corr_test$p.value
+  }
+}
+
+sig_corr <- as.data.frame(correlations)
+
+write.csv(sig_corr, "stk11_mt_sig_rorc_correlations_spearman.csv", row.names = F)
+
